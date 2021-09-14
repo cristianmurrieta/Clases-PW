1089c70dc247bd2178f6f6f732055a4511721fe4
cristianmurrieta <cristianmurriet@gmail.com>
2021-09-13 19:45:42 -0500

    Actualizacion
    Javascript manera moderna funciones arrow

--- Semana 4/index.js
+++ Semana 4/index.js
@@ -1,5 +1,15 @@
 /*document.write("<h1>Hola Javascript</h1>")*/
 
-function clickMeOnClick(){
+/*function clickMeOnClick(){
     document.write("<h1>Hola Javascript </h1>")
+}*/
+
+/*function numeroOnClick(numero){
+    const inputNumeros = document.getElementById("txt_numeros")
+    inputNumeros.value = numero
+}*/
+
+const numeroOnClick = (numero) => {
+    const inputNumeros = document.getElementById("txt_numeros")
+    inputNumeros.value = numero
 }
