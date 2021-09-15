51fe3ce878174dc31321944c5fc6e56955d961af
cristianmurrieta <cristianmurriet@gmail.com>
2021-09-15 17:58:34 -0500

    Actualizacion
    Agregamos operaciones con funciones en js

--- Semana 4/index.js
+++ Semana 4/index.js
@@ -9,7 +9,48 @@
     inputNumeros.value = numero
 }*/
 
+var numeroIngresado;
+var enOperacion = false;
+var operacionActual;
+
+const sumar = (sum1, sum2) =>{
+    return sum1 + sum2;
+}
+
+const restar = (sum1, sum2) =>{
+    return sum1 - sum2;
+}
+
 const numeroOnClick = (numero) => {
     const inputNumeros = document.getElementById("txt_numeros")
-    inputNumeros.value = numero
+    const numeroStr = inputNumeros.value;
+    if(numeroStr == "0" || enOperacion){
+        inputNumeros.value = numero;
+        enOperacion = false;
+    }else{
+        inputNumeros.value = numeroStr + numero;
+    }
+}
+
+const operacionOnClick = (operacion) => {
+    const inputNumeros = document.getElementById("txt_numeros")
+    if(operacion == "c"){
+        inputNumeros.value = "0";
+    }else if(operacion == "+"){
+        const numero = inputNumeros.value;
+        numeroIngresado = parseInt(numero);
+        enOperacion = true;
+        operacionActual = sumar;
+    }else if(operacion == "-"){
+        const numero = inputNumeros.value;
+        numeroIngresado = parseInt(numero);
+        enOperacion = true;
+        operacionActual = restar;
+    }else if(operacion == "="){
+        if(operacionActual != undefined){
+            const numero = inputNumeros.value;
+            const res = operacionActual(numeroIngresado, parseInt(numero))
+            inputNumeros.value = res;
+        }
+    }
 }
